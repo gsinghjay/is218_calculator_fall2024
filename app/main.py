9fab257ebdd623dcb1ecea765b80a801ce25e2de
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2024-09-17 16:52:32 -0400

    added subtraction test

--- app/main.py
+++ app/main.py
@@ -2,3 +2,7 @@
 def addition(a,b):
     ''' Addition Function '''
     return a + b
+
+def subtraction(a,b):
+    ''' Subtraction Function '''
+    return a - b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 '''My Calculator Test'''
-from app.main import addition
+from app.main import addition, subtraction
 
 def test_addition():
     '''Addition Function'''
     assert addition(1,1) == 2
+
+def test_subtraction():
+    '''Subtraction Function'''
+    assert subtraction(1,1) == 0
