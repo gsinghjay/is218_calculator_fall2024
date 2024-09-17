824bfb36f318c7a01e2666904fad1fdc807de698
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2024-09-17 17:18:25 -0400

    Added functions for dividing, dividing by zero, and associated tests

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import pytest
+
 ''' Calculator '''
 def addition(a,b):
     ''' Addition Function '''
@@ -6,3 +8,11 @@ def addition(a,b):
 def subtraction(a,b):
     ''' Subtraction Function '''
     return a - b
+
+def multiplication(a,b):
+    ''' Multiplication Function '''
+    return a * b
+
+def division(a,b):
+    ''' Division Function '''
+    return a / b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 '''My Calculator Test'''
-from app.main import addition, subtraction
+from app.main import addition, subtraction, multiplication, division, pytest
 
 def test_addition():
     '''Addition Function'''
@@ -8,3 +8,17 @@ def test_addition():
 def test_subtraction():
     '''Subtraction Function'''
     assert subtraction(1,1) == 0
+
+def test_multiplication():
+    '''Multiplication Function'''
+    assert multiplication(2,2) == 4
+
+def test_division():
+    '''Division Function'''
+    assert division(2,2) == 1
+
+def test_division_by_zero_exception():
+    '''Division Function testing that I get the exception divide by zero'''
+    with pytest.raises(ZeroDivisionError):
+        division(10, 0)
+        
